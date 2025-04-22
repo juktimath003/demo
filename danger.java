c3a951408fc08c5c6bfe12ed814f9e67bd4bb1d7
juktimath003 <juktimathshreya123@gmail>
2025-04-23 05:27:10 +0530

    save

---
+++ danger.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class danger {
+    public static void main(String[] args) {
+        // Print Hello, World! to the console
+        System.out.println("Hello, World!");
+    }
+}
+
+
